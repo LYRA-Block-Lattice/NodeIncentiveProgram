f54d21de8c4f471e20e8b77fb835e947d5bfe97b
Wizard Young <rcvbuf@gmail.com>
2021-02-07 02:32:34 +0800

    fix

--- NodeIncentiveProgram/Program.cs
+++ NodeIncentiveProgram/Program.cs
@@ -5,15 +5,8 @@ namespace NodeIncentiveProgram
     class Program
     {
         static async System.Threading.Tasks.Task Main(string[] args)
-        {
-            if (args.Length < 1)
-            {
-                Console.WriteLine("need arguments.");
-                return;
-            }
-                
-            var networkId = args[0];
-            Console.WriteLine($"Beginning incentive program for {networkId}...");
+        {              
+            Console.WriteLine($"Beginning incentive program...");
 
             var inc = new IncentivePayer();
             await inc.RunPayAsync();
