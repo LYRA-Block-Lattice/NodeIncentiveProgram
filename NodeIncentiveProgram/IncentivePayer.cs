ffe3a42acd30b4716ed5bbad4fe2cd5ca15c80ab
Wizard Young <rcvbuf@gmail.com>
2022-10-09 15:38:24 +0800

    restore testnet

--- NodeIncentiveProgram/IncentivePayer.cs
+++ NodeIncentiveProgram/IncentivePayer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using LiteDB;
 using Lyra.Core.Accounts;
 using Lyra.Core.API;
 
@@ -32,12 +31,12 @@ namespace NodeIncentiveProgram
             Console.WriteLine($"Incentive wallet {incWallet.AccountId} balance: {incWallet.BaseBalance}\n");
 
             // first lookup for failed payments
-            var dbfn = @"C:\Users\Wizard\OneDrive\dev\lyra\Programs\IncProgram.db";
-            using (var db = new LiteDatabase(dbfn))
-            {
-                var coll = db.GetCollection<IncPayment>("IncPay");
+            //var dbfn = @"C:\Users\Wizard\OneDrive\dev\lyra\Programs\IncProgram.db";
 
-                async Task FixFailedPay()
+            
+                //var coll = db.GetCollection<IncPayment>("IncPay");
+
+                /*async Task FixFailedPay()
                 {
                     var lastPay = coll.FindOne(Query.All(Query.Descending));
 
@@ -45,7 +44,7 @@ namespace NodeIncentiveProgram
 
                     while (true)
                     {
-                        foreach (var nodes in new[] { lastPay.MainnetNodes/*, lastPay.TestnetNodes*/ })
+                        foreach (var nodes in new[] { lastPay.MainnetNodes, lastPay.TestnetNodes })
                         {
                             if (nodes.Count(x => !x.SuccessPaid) == 0)
                             {
@@ -64,7 +63,7 @@ namespace NodeIncentiveProgram
                             }
                         }
                     }
-                }
+                }*/
 
                 //await FixFailedPay();
 
@@ -72,16 +71,16 @@ namespace NodeIncentiveProgram
                 incPay.TimeStamp = DateTime.UtcNow;
 
                 incPay.MainnetNodes = await GetStatusFromNetworkAsync("mainnet");
-                //incPay.TestnetNodes = await GetStatusFromNetworkAsync("testnet");
+                incPay.TestnetNodes = await GetStatusFromNetworkAsync("testnet");
           
-                coll.Insert(incPay);                
+                //coll.Insert(incPay);                
 
                 var index = 1;
                 Console.WriteLine("index SuccessPaid Rito NetworkId AccountId OfflineCount FullyUpgraded IsPrimary PosVotes SharedIP");
                 decimal totalPayment = 0m;
                 decimal package = 400m;
 
-                foreach(var nodes in new[] { incPay.MainnetNodes/*, incPay.TestnetNodes*/ })
+                foreach(var nodes in new[] { incPay.MainnetNodes, incPay.TestnetNodes })
                 foreach (var node in nodes)
                 {
                     await PayNodeAsync(incWallet, node, package);
@@ -92,15 +91,15 @@ namespace NodeIncentiveProgram
                     Console.WriteLine($"No. {index} {resultStr} {node.PaidAmount:n} LYR to {node.NetworkId} {node.AccountId.Substring(0, 10)}... {node.OfflineCount} {node.FullyUpgraded} {node.IsPrimary} {node.PosVotes} {node.SharedIp}");
                     index++;
 
-                    coll.Update(incPay);
+                    //coll.Update(incPay);
                 }
 
                 Console.WriteLine($"Total payment: {totalPayment:n} LYR");
 
                 await Task.Delay(60 * 1000);
 
-                await FixFailedPay();
-            }            
+                //await FixFailedPay();
+       
         }
 
         private async System.Threading.Tasks.Task PayNodeAsync(Wallet wallet, XStatus node, decimal package)
@@ -140,45 +139,39 @@ namespace NodeIncentiveProgram
 
         private async System.Threading.Tasks.Task<List<XStatus>> GetStatusFromNetworkAsync(string networkId)
         {
-            Console.WriteLine($"Getting history data from nebula...");
-            var lr = new LogReader(networkId);
-            var hist = (await lr.GetHistoryAsync())
-                .Where(x => x.TimeStamp > DateTime.UtcNow.AddDays(-1))
-                .ToList();
+            Console.WriteLine($"Getting Billboard data from nebula...");
+            //var lr = new LogReader(networkId);
+            //var hist = (await lr.GetHistoryAsync())
+            //    .Where(x => x.TimeStamp > DateTime.UtcNow.AddDays(-1))
+            //    .ToList();
+
+            var client = LyraRestClient.Create(networkId, "", "", "");
+            var billboard = await client.GetBillBoardAsync();
 
             // first get all accounts
-            var allAccounts = hist.SelectMany(x => x.nodeStatus.Keys).Distinct().ToList();
-            Console.WriteLine($"Got {hist.Count()} history entry for {networkId}. Unique account {allAccounts.Count()}");
+            var allAccounts = billboard.ActiveNodes.Select(a => a.AccountID);
+            Console.WriteLine($"Unique account {allAccounts.Count()}");
 
-            var count = hist.Count;
             var statusList = new List<XStatus>();
             foreach (var acct in allAccounts)
             {
-                var lastState = hist.Last(x => x.nodeStatus.ContainsKey(acct))
-                    .nodeStatus[acct];
-
-                if (lastState == null)
-                    continue;
-
-                var lastBB = hist.Last().bb;
+                var lastBB = billboard;
                 var lastInBB = lastBB.ActiveNodes.FirstOrDefault(x => x.AccountID == acct);
-
+                var state = billboard.ActiveNodes.First(a => a.AccountID == acct).State;
                 var xs = new XStatus
                 {
                     NetworkId = networkId,
                     AccountId = acct,
-                    OfflineCount = count - hist.Count(x => x.nodeStatus.ContainsKey(acct)),
-                    FullyUpgraded = CompareVersion(lastState.Status.version, LyraGlobal.NodeAppName),
+                    OfflineCount = 0,
+                    FullyUpgraded = true,//CompareVersion(lastState.Status.version, LyraGlobal.NodeAppName),
                     IsPrimary = lastBB.PrimaryAuthorizers.Contains(acct),
                     PosVotes = lastInBB == null ? 0 : lastInBB.Votes,
-                    SharedIp = hist.Last(x => x.nodeStatus.ContainsKey(acct))
-                        .bb.NodeAddresses.Count(x => x.Value == hist.Last(x => x.nodeStatus.ContainsKey(acct))
-                        .bb.NodeAddresses[acct]) > 1
+                    SharedIp = false,
+                    IsDbConsist = state == Lyra.Data.API.BlockChainState.Almighty || state == Lyra.Data.API.BlockChainState.Engaging,
+                    //SharedIp = hist.Last(x => x.nodeStatus.ContainsKey(acct))
+                    //    .bb.NodeAddresses.Count(x => x.Value == hist.Last(x => x.nodeStatus.ContainsKey(acct))
+                    //    .bb.NodeAddresses[acct]) > 1
                 };
-
-                // add db consist check
-                xs.IsDbConsist = (lastState.Status.state == Lyra.Data.API.BlockChainState.Almighty
-                    || lastState.Status.state == Lyra.Data.API.BlockChainState.Engaging);
 
                 statusList.Add(xs);
             }
